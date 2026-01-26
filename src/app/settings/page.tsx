bdfa2022bcacc9360258da1264fbe6f2b3d775c1
Ivan Lukan <ivan.lukan17113@gmail.com>
2026-01-26 07:14:57 +0100

    Fixed mobile layout for settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,7 +2,7 @@ import SettingsForm from "@/features/settings/components/SettingsForm";
 
 export default function Settings() {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-dvh">
+    <div className="flex flex-col justify-center items-center w-full min-h-dvh">
       <h1 className="text-7xl font-sans font-semibold text-balance mb-20">
         Settings
       </h1>
