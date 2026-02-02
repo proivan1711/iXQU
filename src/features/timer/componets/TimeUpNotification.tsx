5eaffebaff7616ed59a5362942e3e057c7858b7c
Ivan Lukan <ivan.lukan17113@gmail.com>
2026-02-02 19:18:16 +0100

    Fixed mobile layout for toast

--- src/features/timer/componets/TimeUpNotification.tsx
+++ src/features/timer/componets/TimeUpNotification.tsx
@@ -1,7 +1,7 @@
-import { toast } from "sonner";
-import { Button } from "@/components/ui/button";
-import type { Mode } from "@/contexts/PomodoroContext";
-import { TIMER_NOTIFICATION_ID } from "@/features/timer/config";
+import {toast} from "sonner";
+import {Button} from "@/components/ui/button";
+import type {Mode} from "@/contexts/PomodoroContext";
+import {TIMER_NOTIFICATION_ID} from "@/features/timer/config";
 
 export default function TimeUpNotification({
   restart,
@@ -15,7 +15,7 @@ export default function TimeUpNotification({
       <h1 className="text-balance text-center text-3xl font-bold font-mono min-[600px]:ml-8">
         Time is up!
       </h1>
-      <div className="flex justify-between items-end min-[600px]:gap-8">
+      <div className="flex justify-between items-end gap-8">
         <Button
           onClick={() => {
             toast.dismiss(TIMER_NOTIFICATION_ID);
@@ -24,7 +24,7 @@ export default function TimeUpNotification({
         >
           Exit
         </Button>
-        <div className="flex min-[600px]:flex-col gap-4">
+        <div className="flex flex-col gap-4">
           <Button
             onClick={() => {
               restart(mode === "pomodoro" ? "shortBreak" : "shortBreak");
