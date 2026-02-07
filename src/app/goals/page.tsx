83956fd78ff29f73bcc5bac82e9ab2fd7c7e923e
Ivan Lukan <ivan.lukan17113@gmail.com>
2026-02-07 08:30:12 +0100

    Continue making goals page

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,9 +1,12 @@
+import Goal from "@/features/goals/components/Goal";
+
 export default function Goals() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-7xl md:text-9xl text-balance font-semibold text-center mt-20">
         Goals
       </h1>
+      <Goal />
     </div>
   );
 }
---
+++ src/features/goals/components/Goal.tsx
@@ -0,0 +1,5 @@
+import { Card } from "@/components/ui/card";
+
+export default function Goal() {
+  return <Card></Card>;
+}
