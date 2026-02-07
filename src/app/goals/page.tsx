857f769302a778bf071bd41eb6ade74a88c33a3a
Ivan Lukan <ivan.lukan17113@gmail.com>
2026-02-07 10:25:43 +0100

    Made a progress card partially

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,12 +1,14 @@
-import Goal from "@/features/goals/components/Goal";
+import GoalCard from "@/features/goals/components/Goal";
 
 export default function Goals() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-col items-center justify-start gap-30 w-full h-dvh">
       <h1 className="text-7xl md:text-9xl text-balance font-semibold text-center mt-20">
         Goals
       </h1>
-      <Goal />
+      <div>
+        <GoalCard />
+      </div>
     </div>
   );
 }
--- src/features/goals/components/Goal.tsx
+++ src/features/goals/components/Goal.tsx
@@ -1,5 +1,76 @@
-import { Card } from "@/components/ui/card";
+"use client";
 
-export default function Goal() {
-  return <Card></Card>;
+import {formatDuration} from "date-fns";
+import {Bar, BarChart} from "recharts";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card";
+import {type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent,} from "@/components/ui/chart";
+import {Progress} from "@/components/ui/progress";
+
+const chartConfig = {
+  pomodoros: {
+    label: "Pomodoro Time",
+  },
+} satisfies ChartConfig;
+
+const data = [
+  {
+    day: "Monday",
+    value: 120,
+  },
+  {
+    day: "Tuesday",
+    value: 150,
+  },
+  {
+    day: "Wednesday",
+    value: 90,
+  },
+  {
+    day: "Thursday",
+    value: 180,
+  },
+  {
+    day: "Friday",
+    value: 160,
+  },
+  {
+    day: "Saturday",
+    value: 100,
+  },
+  {
+    day: "Sunday",
+    value: 75,
+  },
+];
+
+export default function GoalCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl font-semibold">
+          Full stack web developer
+        </CardTitle>
+        <CardDescription>Goal</CardDescription>
+      </CardHeader>
+      <CardContent className="gap-4 flex flex-col">
+        <ChartContainer config={chartConfig} className="w-70">
+          <BarChart accessibilityLayer data={data}>
+            <ChartTooltip
+              content={
+                <ChartTooltipContent className="w-30" nameKey="pomodoros" />
+              }
+              formatter={(time: number) => {
+                return formatDuration({
+                  minutes: time % 60,
+                  hours: Math.floor(time / 60),
+                });
+              }}
+            />
+            <Bar dataKey={"value"}></Bar>
+          </BarChart>
+        </ChartContainer>
+        <Progress value={30} />
+      </CardContent>
+    </Card>
+  );
 }
