d903fdaa3bb5a950f00cce591c5a75994aaa9b05
Ivan Lukan <ivan.lukan17113@gmail.com>
2026-02-08 09:24:27 +0100

    Fixed accessibility for pomodoro timer buttons

--- src/features/timer/componets/TimerPlayControls.tsx
+++ src/features/timer/componets/TimerPlayControls.tsx
@@ -16,21 +16,30 @@ export default function TimerPlayControls() {
 
   return (
     <div className="-mb-10 mt-10 border-border rounded-2xl px-3 py-2 sm:px-6 sm:py-4 md:px-9 md:py-6 border grid grid-cols-[1fr_5fr] md:grid-cols-[1fr_6fr] justify-center items-center">
-      <Button size="icon-xl" onClick={() => restart()}>
+      <Button
+        size="icon-xl"
+        onClick={() => restart()}
+        aria-label="Restart timer"
+      >
         <RotateCw />
       </Button>
       <div className="flex w-full justify-center items-center gap-2 md:gap-4">
-        <Button size="icon-xl" onClick={skipBackward}>
+        <Button
+          size="icon-xl"
+          onClick={skipBackward}
+          aria-label="Skip backward"
+        >
           <ArrowBigLeftDash />
         </Button>
         <Button
+          aria-label={isRunning ? "Stop timer" : "Start timer"}
           size="icon-4xl"
           className="rounded-full"
           onClick={isRunning ? pause : resume}
         >
           {isRunning ? <Pause /> : <Play />}
         </Button>
-        <Button size="icon-xl" onClick={skip}>
+        <Button size="icon-xl" onClick={skip} aria-label="Skip forward">
           <ArrowBigRightDash />
         </Button>
       </div>
